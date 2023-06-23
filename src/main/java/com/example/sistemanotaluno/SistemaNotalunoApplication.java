b6841a615ad36af7e7767e89070730ef88f7f15b
IanOSilva763 <ian.o.s@hotmail.com>
2023-06-23 16:28:00 -0300

    first commit

---
+++ src/main/java/com/example/sistemanotaluno/SistemaNotalunoApplication.java
@@ -0,0 +1,13 @@
+package com.example.sistemanotaluno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaNotalunoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaNotalunoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sistemanotaluno/SistemaNotalunoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sistemanotaluno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaNotalunoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
